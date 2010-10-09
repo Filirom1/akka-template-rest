97d9bfcb700d292dd25a58085eddb4bdeb62954e
Romain <romain@romain-desktop.(none)>
2010-10-09 16:24:58 +0200

    update to akka 0.10 and scala 2.8.0

--- src/main/scala/samples/HelloService.scala
+++ src/main/scala/samples/HelloService.scala
@@ -1,19 +1,42 @@
-package samples
+/**
+ * Copyright (C) 2009-2010 Scalable Solutions AB <http://scalablesolutions.se>
+ */
 
-import se.scalablesolutions.akka.actor.Actor
+package sample.rest.scala
 
 import javax.ws.rs.{GET, Path, Produces}
+import se.scalablesolutions.akka.actor.{SupervisorFactory, Actor}
+import se.scalablesolutions.akka.config.ScalaConfig._
+
+class Boot {
+  	val factory = SupervisorFactory(
+		SupervisorConfig(
+			RestartStrategy(OneForOne, 3, 100, List(classOf[Exception])),
+			Supervise(Actor.actorOf[MyActor], LifeCycle(Permanent)) :: Nil))
+	factory.newInstance.start
+}
+
+private case object Hello
 
 @Path("/hello")
-class HelloService extends Actor {
+class HelloService {
+  @GET
+  @Produces(Array("text/html"))
+  def hello = {
+    val myActor = Actor.actorOf[MyActor]
+    //I don't understand why SupervisorFactory do not start the actor ?????
+    myActor.start
+    (myActor !! Hello).getOrElse("Hyyy")
 
-	private case object Hello
+  }
 
-	@GET
-	@Produces(Array("text/html"))
-	def hello = (this !! Hello).getOrElse("couldn't say hello")
 
-	def receive = {
-		case Hello => reply(<h1>Hello, World</h1>.toString)
-	}
+}
+
+class MyActor extends Actor {
+  import self._
+
+  def receive = {
+    case Hello => reply(<h1>Hello, World</h1>.toString)
+  }
 }
