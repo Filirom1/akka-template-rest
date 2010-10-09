2b5ddbd71f471232c342d3542b2a6f54cdb6f478
Romain <romain@romain-desktop.(none)>
2010-10-09 17:31:12 +0200

    Use of ActorRegistry.actorFor[MyActor].get instead of Actor.actorOf[MyActor]
    Now I do not need to start the actor manually.
    
    http://groups.google.com/group/akka-user/browse_thread/thread/daaef5c4fa25cb6d

--- src/main/scala/samples/HelloService.scala
+++ src/main/scala/samples/HelloService.scala
@@ -5,15 +5,14 @@
 package sample.rest.scala
 
 import javax.ws.rs.{GET, Path, Produces}
-import se.scalablesolutions.akka.actor.{SupervisorFactory, Actor}
 import se.scalablesolutions.akka.config.ScalaConfig._
-
+import se.scalablesolutions.akka.actor.{ActorRegistry, SupervisorFactory, Actor}
 class Boot {
-  	val factory = SupervisorFactory(
-		SupervisorConfig(
-			RestartStrategy(OneForOne, 3, 100, List(classOf[Exception])),
-			Supervise(Actor.actorOf[MyActor], LifeCycle(Permanent)) :: Nil))
-	factory.newInstance.start
+  val factory = SupervisorFactory(
+    SupervisorConfig(
+      RestartStrategy(OneForOne, 3, 100, List(classOf[Exception])),
+      Supervise(Actor.actorOf[MyActor], LifeCycle(Permanent)) :: Nil))
+  factory.newInstance.start
 }
 
 private case object Hello
@@ -23,14 +22,10 @@ class HelloService {
   @GET
   @Produces(Array("text/html"))
   def hello = {
-    val myActor = Actor.actorOf[MyActor]
-    //I don't understand why SupervisorFactory do not start the actor ?????
-    myActor.start
+    val myActor = ActorRegistry.actorFor[MyActor].get
     (myActor !! Hello).getOrElse("Hyyy")
 
   }
-
-
 }
 
 class MyActor extends Actor {
